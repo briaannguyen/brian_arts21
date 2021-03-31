d211b9fbab02bc6d96d4f8e535890990716b17af
briaannguyen <78726319+briaannguyen@users.noreply.github.com>
2021-03-31 02:13:52 -0400

    jquery commit

---
+++ exercises/exercises/jquery/assets/main.js
@@ -0,0 +1,42 @@
+$(function() {
+  $(".button1").click(function() {
+    $("body").toggleClass("sunrise");
+  });
+});
+
+$(function() {
+  $(".button2").click(function() {
+    $("body").toggleClass("morning");
+  });
+});
+
+$(function() {
+  $(".button3").click(function() {
+    $("body").toggleClass("daytime");
+  });
+});
+
+$(function() {
+  $(".button4").click(function() {
+    $("body").toggleClass("sunset");
+  });
+});
+
+$(function() {
+  $(".button5").click(function() {
+    $("body").toggleClass("evening");
+  });
+});
+
+$(function() {
+  $(".button6").click(function() {
+    $("body").toggleClass("nighttime");
+  });
+});
+
+
+$(function() {
+  $(".button7").click(function() {
+    $("body").toggleClass("dreamtime");
+  });
+});
